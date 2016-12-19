61aa071dfc268e30ed0e58176eb7d5f09a227d41
Pratheek Devaraj <pratheekdevaraj@gmail.com>
2016-12-18 17:09:01 -0800

    attempting to pass travis 3

--- sd_py/sd.py
+++ sd_py/sd.py
@@ -1,15 +1,11 @@
+#calculates standard deviation from a list of numeric values
 def standard_deviation(x):
-
-	"""
-	This function calculates the standard deviation of a list.
-	Input: list of numeric values.
-	Output: standard deviation.
-	"""
     n = len(x)
     mean = sum(x)/n
     ssq = sum((x_i-mean)**2 for x_i in x)
     stdev = (ssq/n)**0.5
     return(stdev)
-    
+
+#calcualtes standard error from a list of values	
 def standard_error(x):
     return(standard_deviation(x)/len(x)**0.5)
